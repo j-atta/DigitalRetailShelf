db99c53318267b974a2f02350b66921e585adc1d
joel_atta <joel.atta@outlook.com>
2023-10-14 08:06:55 +1100

    Individual Values & Basic Formatting
    - Individual values can now be pulled from the database
    - This allows for individual manipulation of those values within the .ino code
    - Applied some basic formatting to the ticket

--- RetailShelf.ino
+++ RetailShelf.ino
@@ -13,31 +13,24 @@
 
 // GLOBAL VARIABLES
 GxEPD2_BW<GxEPD2_213_B74, GxEPD2_213_B74::HEIGHT> display(GxEPD2_213_B74(SS, 17, 16, 4));
-const char TextToDisplay[] = "";
 const char* ssid = "NetComm 9343";
 const char* password = "harelagehi";
-String check = "check_product_info";
-
 String URL = "http://192.168.20.18/retailshelf/index.php";
 
+// INFO FOR DISPLAY
+// const char productName[];
+// const char productBrand[];
+// int productPrice;
+// int productPromoPrice;
+// int productPromoStatus;
+// int productSOH;
+
 void setup() {
-  // put your setup code here, to run once:
+  // Initialisation
   Serial.begin(115200);
-  setupWifi(ssid, password);
-  HTTPClient http;
-  http.begin(URL);
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  int response_code = http.POST(check);
-
-  if(response_code > 0){
-        Serial.println("HTTP code " + String(response_code));
-  }
-
-  if(response_code == 200) {
-    Serial.print("Valid");
-  }
   display.init(115200, true, 2, false);
-  printText(http.getString().c_str());
+  setupWifi(ssid, password);
+  printText();
 }
 
 void loop() {
@@ -50,22 +43,20 @@ void loop() {
    }
 } 
 
-void printText(const char message[]) {
+void printText() {
   display.setRotation(1);
   display.setFont(&FreeMonoBold9pt7b);
   display.setTextColor(GxEPD_BLACK);
-  int16_t tbx, tby; uint16_t tbw, tbh;
-  display.getTextBounds(message, 0, 0, &tbx, &tby, &tbw, &tbh);
-  // center the bounding box by transposition of the origin:
-  uint16_t x = ((display.width() - tbw) / 2) - tbx;
-  uint16_t y = ((display.height() - tbh) / 2) - tby;
-  display.setFullWindow();
   display.firstPage();
   do
   {
     display.fillScreen(GxEPD_WHITE);
-    display.setCursor(x, y);
-    display.print(message);
+    display.setCursor(0, 20);
+    display.println(String("SKU: " + getProductNumberFromServer()).c_str());
+    display.println(String("Name: " + getProductNameFromServer()).c_str());
+    display.println(String("Brand: " + getProductBrandFromServer()).c_str());
+    display.println(String("Price: $" + getProductPriceFromServer()).c_str());
+
   }
   while (display.nextPage());
 }
@@ -77,4 +68,67 @@ void setupWifi(const char* initName, const char* initPassword ) {
     delay(500);
     Serial.print(".");
   }
+}
+
+String getProductNumberFromServer() {
+  HTTPClient http;
+  http.begin(URL);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int response_code = http.POST("get_product_no");
+
+  return http.getString();
+}
+
+String getProductNameFromServer() {
+  HTTPClient http;
+  http.begin(URL);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int response_code = http.POST("get_product_name");
+
+  return http.getString();
+}
+
+String getProductBrandFromServer() {
+  HTTPClient http;
+  http.begin(URL);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int response_code = http.POST("get_product_brand");
+  
+  return http.getString();
+}
+
+String getProductPriceFromServer() {
+  HTTPClient http;
+  http.begin(URL);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int response_code = http.POST("get_product_price");
+  
+  return http.getString();
+}
+
+String getProductPromoStatusFromServer() {
+  HTTPClient http;
+  http.begin(URL);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int response_code = http.POST("get_product_promo_status");
+
+  return http.getString();
+}
+
+String getProductPromoPriceFromServer() {
+  HTTPClient http;
+  http.begin(URL);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int response_code = http.POST("get_product_promo_price");
+  
+  return http.getString();
+}
+
+String getProductSohFromServer() {
+  HTTPClient http;
+  http.begin(URL);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  int response_code = http.POST("get_product_soh");
+  
+  return http.getString();
 }
--- index.php
+++ index.php
@@ -9,22 +9,104 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-  }
-  echo "Connected successfully";
+}
 
-  if (isset($_POST['check_product_info'])) {
-    $sql = "SELECT * FROM products WHERE prod_no = 50079832";
-    $result = $conn->query($sql);
-  
-    if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-        echo $row["prod_name"];
-      }
-    } else {
-      echo "0 results";
+if (isset($_POST['get_product_no'])) {
+  $sql = "SELECT prod_no FROM products WHERE prod_no = 50079832";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo $row["prod_no"];
     }
-    $conn->close();
+  } else {
+    echo "0 results";
   }
+}
+
+if (isset($_POST['get_product_name'])) {
+  $sql = "SELECT prod_name FROM products WHERE prod_no = 50079832";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo $row["prod_name"];
+    }
+  } else {
+    echo "0 results";
+  }
+}
+
+if (isset($_POST['get_product_brand'])) {
+  $sql = "SELECT prod_brand FROM products WHERE prod_no = 50079832";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo $row["prod_brand"];
+    }
+  } else {
+    echo "0 results";
+  }
+}
+
+if (isset($_POST['get_product_price'])) {
+  $sql = "SELECT prod_price FROM products WHERE prod_no = 50079832";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo $row["prod_price"];
+    }
+  } else {
+    echo "0 results";
+  }
+}
+
+if (isset($_POST['get_product_promo_status'])) {
+  $sql = "SELECT prod_promo_status FROM products WHERE prod_no = 50079832";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo $row["prod_promo_status"];
+    }
+  } else {
+    echo "0 results";
+  }
+}
+
+if (isset($_POST['get_product_promo_price'])) {
+  $sql = "SELECT prod_promo_price FROM products WHERE prod_no = 50079832";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo $row["prod_promo_price"];
+    }
+  } else {
+    echo "0 results";
+  }
+}
+
+if (isset($_POST['get_product_soh'])) {
+  $sql = "SELECT prod_soh FROM products WHERE prod_no = 50079832";
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo $row["prod_soh"];
+    }
+  } else {
+    echo "0 results";
+  }
+}
 
 ?>
